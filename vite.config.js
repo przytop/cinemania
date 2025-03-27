33336fd6b9f6271aee1d6f6e7ad1a4765d36f191
Pawel Przytomski <przytop@gmail.com>
2025-03-28 00:01:27 +0100

    Redeploy
    - Reverted Vite version to 5.4.6 due to issues with newer versions
    - Updated other dependencies
    - Fixed issues with image URLs
    - Edited Vite config to fix minor build errors

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import { sync } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import commonjs from '@rollup/plugin-commonjs';
@@ -13,7 +13,7 @@ export default defineConfig(({ command }) => {
     build: {
       sourcemap: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input: sync('./src/*.html'),
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -25,7 +25,7 @@ export default defineConfig(({ command }) => {
         external: [],
         plugins: [commonjs()],
       },
-      outDir: '../dist',
+      outDir: 'dist',
     },
     optimizeDeps: {
       include: [],
